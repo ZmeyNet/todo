c40502d711c64d8d737096731714860933032adc
andrew.zabirko <Andrew.Zabirko@apps.ask.com>
2021-04-25 21:03:19 +0300

    Initial commit ToDo webAPI; added default gitIgnore file for VS; Initial initialization done for WEB APi project template; lib added Swagger; lib added EF core;

---
+++ WebToDoAPI/Data/InitailSeedDB.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebToDoAPI.Data
+{
+    public class InitailSeedDB
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetRequiredService<ToDoDbContext>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            context.Database.EnsureCreated();
+            
+            if (!context.Users.Any())
+            {
+                ApplicationUser user = new ApplicationUser()
+                {                    
+                    Email = "user@example.com",
+                    UserName = "user",
+                    SecurityStamp = Guid.NewGuid().ToString()
+                };
+                ApplicationUser lockedUser = new ApplicationUser()
+                {
+                    Email = "user1@example.com",
+                    UserName = "user1",
+                    SecurityStamp = Guid.NewGuid().ToString()
+                };
+                ApplicationUser userWithAdminRights = new ApplicationUser()
+                {
+                    Email = "admin@example.com",
+                    UserName = "adimn",
+                    SecurityStamp = Guid.NewGuid().ToString()
+                };
+
+                userManager.CreateAsync(user, "pwd@@123");
+                userManager.CreateAsync(lockedUser, "pwd@@123");
+                userManager.CreateAsync(userWithAdminRights, "pwd@@123");
+            }
+        }
+
+    }
+}
---
+++ WebToDoAPI/Data/ToDoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebToDoAPI.Data
+{
+    public class ToDoDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
